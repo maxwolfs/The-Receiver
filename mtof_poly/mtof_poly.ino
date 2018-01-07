7c12670aff7e434ac0dec11be15ec62d218e1214
Maximilian Wolfs <max.wolfs@gmx.de>
2018-01-07 20:54:56 +0100

    ADD polyphonic code

---
+++ mtof_poly/mtof_poly.ino
@@ -0,0 +1,92 @@
+/*  Polyphonic Pin Printer Synthesizer by Maximilian Wolfs. (c) Copyright 2017
+    Midi Input -> Midi to Frequency Interval -> Output
+*/
+
+#include <MIDI.h>
+#include <elapsedMillis.h>
+
+elapsedMicros timeElapsed1;
+elapsedMicros timeElapsed2;
+elapsedMicros timerArray[] = {&timeElapsed1,&timeElapsed2};
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+int solPin[] = {3, 4}; // Solenoid Pins
+#define solCnt 2 // total of active Solenoids
+
+int intvlArray[solCnt]; // array to save pitches
+byte rawPitch[solCnt];
+
+int counter = 0; // counter that increments per handleNoteOn
+boolean solState[] = {LOW, LOW};  // init state of Solenoids
+boolean isPlaying[] = {false, false}; // init state of Solenoids
+byte pitch; //
+float midival;
+float intvl;
+float freq;
+
+float mtof(float pitch)
+{
+  float f = 0.0;
+  if (pitch) f = 8.1757989156 * pow(2.0, pitch / 12.0);
+  return f;
+}
+
+float inter(float freq)
+{
+  float f = 1 / freq * 1000000.0 / 2.0;
+  return f;
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity)
+{
+  rawPitch[counter % solCnt] = pitch;
+  freq = mtof(float(pitch));
+  intvl = inter(freq);
+  intvlArray[counter % solCnt] = intvl;
+  timerArray[counter % solCnt] = 0;
+  isPlaying[counter % solCnt] = true;
+  counter++;
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity)
+{
+  for (int i = 0; i < solCnt; i++) {
+    if (rawPitch[i] == pitch) {
+      digitalWrite(solPin[i], solState[i]);
+      isPlaying[i] = false;
+    }
+  }
+}
+
+void playNote()
+{
+  for (int i = 0; i < solCnt; i++)
+  {
+    if (isPlaying[i]) {
+      if (timerArray[i] >= intvlArray[i]) {
+        timerArray[i] = 0;
+        solState[i] = !solState[i];
+        digitalWrite(solPin[i], solState[i]);
+      }
+    } else digitalWrite(solPin[i], LOW);
+  }
+}
+
+void setup()
+{
+  for (int i = 0; i < solCnt; i++)
+  {
+    pinMode(solPin[i], OUTPUT);
+  }
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.begin();
+}
+
+void loop()
+{
+  MIDI.read();
+  playNote();
+}
+
